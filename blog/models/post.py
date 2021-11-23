0fbd266ae65a89c58510287f7c500744af46637c
sirjandrn25 <sirjantmg99@gmail.com>
2021-11-23 21:44:18 +0545

    change git repository

---
+++ blog/models/post.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+
+class DateTimePicker(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering=['-id']
+
+
+class Category(models.Model):
+    c_name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.c_name
+
+class Tutorial(models.Model):
+    name = models.CharField(max_length=150)
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="tutorials")
+
+    def __str__(self):
+        return self.name
+    
+
+class Post(DateTimePicker):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to="posts",blank=True,null=True)
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="posts")
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="posts")
+    likes = models.ManyToManyField('auth.User',blank=True,related_name="likes")
+    views = models.ManyToManyField('auth.User',blank=True,related_name="views")
+    tutorial = models.ForeignKey(Tutorial,on_delete=models.CASCADE,related_name="posts",null=True)
+
+    def __str__(self):
+        return self.title
+    
+    @staticmethod
+    def autocomplete_search_fields():
+        return 'title', 'user__name'
+    class Meta:
+        ordering = ['id']
+
+    
+
+
+
+class Comment(DateTimePicker):
+    content = models.TextField()
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="comments")
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name="comments")
+
+    class Meta:
+        ordering = ['id']
+
+    
+
+class Reply(DateTimePicker):
+    content = models.TextField()
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE,related_name="replies")
+    comment = models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="replies")
+    class Meta:
+        ordering = ['id']
---
+++ blog/permissions.py
@@ -0,0 +1,44 @@
+from rest_framework.permissions import BasePermission
+from rest_framework import permissions
+from .models import *
+
+class IsPostOwnerOrIsAdmin(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        # print(permissions.SAFE_METHODS)
+        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+            return True
+        return request.user == obj.user
+    
+class IsPostOwnerOrCommentOwnerOrIsAdmin(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        post = Post.objects.filter(pk=obj.post.id).first()
+        if request.method in permissions.SAFE_METHODS or request.user.is_superuser or request.user==post.user:
+            return True
+        return request.user == obj.user
+
+
+
+class IsPostOwnerOrReplyOwnerOrIsAdmin(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        post = Post.objects.filter(pk=obj.comment.post.id).first()
+        if request.method in permissions.SAFE_METHODS or request.user.is_superuser or request.user==post.user:
+            return True
+        return request.user == obj.user
+
+class IsOwnUserOrIsAdmin(BasePermission):
+    def has_object_permission(self,request,view,obj):
+        if request.method == 'DELETE':
+            return request.user.is_superuser
+        else:
+            if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+                return True
+            return request.user == obj
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self,request,view):
+        
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_superuser
+                
+        
