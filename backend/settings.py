de50fbaeaffbd882765ad964bbcf8b27849a8873
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-19 19:36:31 +0545

    change url,hide secrete key and change database

--- backend/settings.py
+++ backend/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-
-
 # import environ
 
 # env = environ.Env()
@@ -23,23 +21,16 @@ from decouple import config
 # environ.Env.read_env()
 #...
 #...rest will be same
-
+SECRET_KEY = config("SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY") 
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # "blog-api7991.herokuapp.com",
-ALLOWED_HOSTS = ["blog-api7991.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["blog2-api7991.herokuapp.com","localhost"]
 # ,
 
 # Application definition
