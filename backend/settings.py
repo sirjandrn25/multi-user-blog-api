19d5ff2fa3ac6d13c8e5a6823744c1e5f42cf9f4
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-19 21:08:50 +0545

    changes secrete key

--- backend/settings.py
+++ backend/settings.py
@@ -22,7 +22,7 @@ import dj_database_url
 # reading .env file
 # environ.Env.read_env()
 #...
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
